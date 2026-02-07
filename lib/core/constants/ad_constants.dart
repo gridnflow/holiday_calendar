7670da7cda17d7fdb6a4de5d27bc4e38e88321dd
yeong.wi <wi10004ok@gmail.com>
2026-02-07 17:27:09 +0100

    add privacy policy doc

--- lib/core/constants/ad_constants.dart
+++ lib/core/constants/ad_constants.dart
@@ -12,10 +12,9 @@ class AdConstants {
   static String get bannerAdUnitId {
     if (kIsWeb) return '';
     if (defaultTargetPlatform == TargetPlatform.android) {
-      // Test Ad Unit ID for Android
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-6139362725426823/5208063091';
     } else if (defaultTargetPlatform == TargetPlatform.iOS) {
-      // Test Ad Unit ID for iOS
+      // TODO: Replace with actual iOS Ad Unit ID
       return 'ca-app-pub-3940256099942544/2934735716';
     }
     return '';
@@ -24,14 +23,12 @@ class AdConstants {
   static String get nativeAdUnitId {
     if (kIsWeb) return '';
     if (defaultTargetPlatform == TargetPlatform.android) {
-      return 'ca-app-pub-3940256099942544/2247696110';
+      return 'ca-app-pub-6139362725426823/3703409736';
     } else if (defaultTargetPlatform == TargetPlatform.iOS) {
       return 'ca-app-pub-3940256099942544/3986624511';
     }
     return '';
   }
 
-  // TODO: Replace with your actual AdMob App ID before release
-  // Android: ca-app-pub-XXXXXXXXXXXXXXXX~XXXXXXXXXX
-  // iOS: ca-app-pub-XXXXXXXXXXXXXXXX~XXXXXXXXXX
+  // Android App ID: ca-app-pub-6139362725426823~2992119401
 }
